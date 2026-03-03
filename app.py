b34e2c6df8a7e15672668fa8baeb3cf0426cbf43
Om Raju Bankar <ombankar65@gmail.com>
2026-03-03 19:47:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,43 +8,27 @@ app = Flask(__name__)
 # Load model safely
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, "model.pkl")
-
 model = pickle.load(open(model_path, "rb"))
 
-
-# Home Route
 @app.route("/")
 def home():
-    return """
-    <h2>Model is running successfully!</h2>
-    <p>To test prediction:</p>
-    <p>/predict?feature1=10&feature2=20</p>
-    """
+    return "Model is running successfully!"
 
-
-# Prediction Route (Browser Friendly)
 @app.route("/predict")
 def predict():
     try:
-        # Get values from URL
         feature1 = float(request.args.get("feature1"))
         feature2 = float(request.args.get("feature2"))
 
-        # Convert into numpy array
         features = np.array([[feature1, feature2]])
-
-        # Predict
         prediction = model.predict(features)
 
-        return jsonify({
-            "feature1": feature1,
-            "feature2": feature2,
-            "prediction": prediction.tolist()
-        })
+        return jsonify({"prediction": prediction.tolist()})
 
     except Exception as e:
         return jsonify({"error": str(e)})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
